fbdf0cf5273373e0637d1671f67dd92cafbdd298
seemingyuan <37139855+seemingyuan@users.noreply.github.com>
2023-02-11 17:48:10 +0800

    Update ENITIO_const.h

--- WANDERER_main/ENITIO_const.h
+++ WANDERER_main/ENITIO_const.h
@@ -15,7 +15,7 @@ const int CreditProcess = 6 ;
 #define EEPROM_SIZE 20
 
 const int PROFILE_enable_add = 0;
-const int OG_add = 1;
+const int CLAN_add = 1;
 const int NAME_add = 2;
 const int isGL_add = 3;
 const int ID_add = 4;
@@ -24,13 +24,13 @@ const int PLAYER_HP_add = 6;
 const int PLAYER_EN_add = 7;
 const int PLAYER_MaxHP_add = 8;
 const int PLAYER_MaxEn_add = 9;
-const int PLAYER_MANA_add = 10;
+const int PLAYER_MULTIPLIER_add = 10;
 const int PLAYER_numKilled_add = 11;
 const int PLAYER_numL1Treasure_add = 12;
 const int PLAYER_numL2Treasure_add = 13;
 const int PLAYER_num_bonus6HP_add = 14;
 const int PLAYER_num_bonus1MaxEn_add = 15;
-const int PLAYER_num_bonus1MANA_add = 16;
+const int PLAYER_num_bonus1MULTIPLIER_add = 16;
 const int PLAYER_num_fiveminx2EnRegen_add = 17;
 const int PLAYER_num_bomb_add = 18;
 
@@ -43,15 +43,18 @@ int PARTICIPANT_MaxHP;
 int GL_MaxHP;
 int PARTICIPANT_MaxEn;
 int GL_MaxEn;
-int INITIAL_MANA;
-int HEAL_MANA;
-int MAX_ATTACK_MANA;
-int MAX_COLLECT_MANA;
+int INITIAL_MULTIPLIER;
+int MAX_ATTACK_MULTIPLIER;
+int MAX_COLLECT_MULTIPLIER;
 int BOMB_HP_DEDUCTION;
 int KILL_UPDATE_SERVER_INTERVAL;
+int revival_MULTIPLIER;
+
 
 int failed_kill_feedback = 0;
-int failed_kill_OG[50] = {};
+int failed_kill_CLAN[50] = {};
 int failed_kill_ID[50] = {};
 int current_failed_save_pointer = 0;
 int current_failed_read_pointer = 0;
+
+
