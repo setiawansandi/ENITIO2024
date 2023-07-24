a104a09570264b881235a074b32b37afa5646de8
Chan Kah Leong <chankahleong@outlook.com>
2023-07-24 17:02:32 +0800

    Deleted removed function codes

--- RevivalStation/ENITIO_wifi.h
+++ RevivalStation/ENITIO_wifi.h
@@ -2,7 +2,6 @@
 #include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
-#include <esp_now.h>
 
 // WiFi Based on https://github.com/martinius96/ESP32-eduroam/blob/master/2022/eduroam/eduroam.ino
 
@@ -16,157 +15,127 @@ const char *ssid = "NTUSECURE";
 int wifi_reconnect_counter = 0;
 int HTTP_TIMEOUT = 30 * 1000;
 
-const char* root_ca PROGMEM = \
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIDljCCAn6gAwIBAgIQNVdBpOD85JGeYm6zWCKCmjANBgkqhkiG9w0BAQsFADBI\n" \
-"MRswGQYDVQQDDBJFU0VUIFNTTCBGaWx0ZXIgQ0ExHDAaBgNVBAoME0VTRVQsIHNw\n" \
-"b2wuIHMgci4gby4xCzAJBgNVBAYTAlNLMB4XDTIzMDIyNzE1NTU1N1oXDTIzMDUy\n" \
-"ODE1NTU1NlowIjEgMB4GA1UEAxMXZW5pdGlvdHJlYXN1cmVodW50LmxpbmswggEi\n" \
-"MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDXIa37ToQjxiy8koxWUw0Rpzdv\n" \
-"/11odPGgIbMGpT05DD/UNK9CaxTfJLw3ftT4EEcPHFzavvTXG7uJ6OOtXfi0WIbf\n" \
-"fgiKD/ve8ch6oc9Vqp7HbFWZ0cg3JKFwkGw5MF2JO6vTkzbzht97dj4UAj5FMh8d\n" \
-"EGPFMR7plbf8P5RGuQCdzrPk+i3/6N/jo6bSCYr1hqxCMQgeGkTLFr+F7I7emkdQ\n" \
-"JcOfaqAcJalnKS39PDqf44bhza2xz2cxKllgS4fxCKJQH/ApyTaHDMLHPlhMNStj\n" \
-"uf6WDs6c1K8XIj5AmGYvFxuRyhzmcbdAG2hv3WyPGjcDAeu53LK/wejj/h05AgMB\n" \
-"AAGjgaEwgZ4wCwYDVR0PBAQDAgWgMB0GA1UdDgQWBBQtHxvNJX/jCsJbVPZZFiai\n" \
-"0ndfqTAfBgNVHSMEGDAWgBR1fBmAqE/V8PIcSLwG0TBEt6AnrTAdBgNVHSUEFjAU\n" \
-"BggrBgEFBQcDAQYIKwYBBQUHAwIwDAYDVR0TAQH/BAIwADAiBgNVHREEGzAZghdl\n" \
-"bml0aW90cmVhc3VyZWh1bnQubGluazANBgkqhkiG9w0BAQsFAAOCAQEAuThNYcXz\n" \
-"VVMGPYNICi/puKYGxkiS019soTbIwRjq3dNKcWF4sB/ba2OjioM7UCnJ1TE9uEGO\n" \
-"vkjj7gbSIcJakfv0idFrj5i3q9Kgo3bnpqcm+LdpEo6+FvR+Lra+ptxG8lqj1p4l\n" \
-"luth4Pf6Sf1Ck2iPgjIoTDtdO/xW+0oF2pG9dEAFnHMabjS6F+CpR126SHv80gcW\n" \
-"D86l8F9Z5/r7mTwGv2K5B6OiTuxV1UkbxRfHlzosVkRTu1nvOKxuwS/6do1spy6F\n" \
-"IZXVoS81d2f+4CTAnwDLajUUcL27i/UD+nVhHaR2vs+WQcZ2FrIlXVsoXdlgr+ko\n" \
-"nDWRvXqKgJnnMA==\n" \
-"-----END CERTIFICATE-----\n";
-
 struct GAME_CONSTANTS {
-    int REVIVAL_STATION_INITIAL_HP;
-    int REVIVAL_STATION_ACTION_RECV_WAIT;
-    int REVIVAL_STATION_RECOVER_DURATION;
-    int REVIVAL_STATION_MULTIPLIER;
-    int HTTP_TIMEOUT;
+  int REVIVAL_STATION_INITIAL_HP;
+  int REVIVAL_STATION_ACTION_RECV_WAIT;
+  int REVIVAL_STATION_RECOVER_DURATION;
+  int REVIVAL_STATION_MULTIPLIER;
+  int HTTP_TIMEOUT;
 };
 
 class DBConnection {
-    private:
-        String DATABASE_URL = "https://enitiotreasurehunt.link/";
-        String DATABASE_DEBUG_URL = "http://192.168.0.114:5000/";
-        bool DEBUG = false;
-        String GET_Request(String route) {
-            HTTPClient http;
-            String url;
-            http.setTimeout(HTTP_TIMEOUT);
-            if (!DEBUG) {
-                WiFiClientSecure https_client;
-                https_client.setCACert(root_ca);
-                https_client.setInsecure();
-                url = DATABASE_URL + route;
-                http.begin(https_client, url.c_str());
-            } else {
-                url = DATABASE_DEBUG_URL + route;
-                Serial.print("DEBUG GET "); Serial.println(url);
-                http.begin(url.c_str());
-            }
-            
-            int httpResponseCode = http.GET();
-            String payload = "{}";
-        
-            if (httpResponseCode > 0) {
-                Serial.print("HTTP Response code: "); Serial.println(httpResponseCode);
-                payload = http.getString();
-            } else {
-                Serial.print("Error code: "); Serial.println(httpResponseCode);
-            }
-            http.end();
-            return payload;
-        };
-    
-        String retrieveParameterFromJSONArray(String parameter, String json_array) {
-            JSONVar json_obj = JSON.parse(json_array);
-            if (JSON.typeof(json_obj) == "undefined") {
-                Serial.println("Parsing input failed!");
-                return "";
-            }
-            return JSON.stringify(json_obj[parameter]);
-        };
-        
-        String POST_Request(String route, const char* payload) {
-            HTTPClient http;
-            http.setTimeout(HTTP_TIMEOUT);
-            String url;
-            if (!DEBUG) {
-                WiFiClientSecure https_client;
-                https_client.setCACert(root_ca);
-                https_client.setInsecure();
-                url = DATABASE_URL + route;
-                http.begin(https_client, url.c_str());
-            } else {
-                url = DATABASE_DEBUG_URL + route;
-                Serial.print("DEBUG POST "); Serial.println(url);
-                http.begin(url.c_str());
-            }
-            Serial.println(url);
-            http.addHeader("Content-Type", "application/json");
-            int httpResponseCode = http.POST(payload);
-            String responsePayload = "{}";
-            if (httpResponseCode > 0) {
-                Serial.print("HTTP Response code: "); Serial.println(httpResponseCode);
-                responsePayload = http.getString();
-            }
-            else {
-                Serial.print("Error code: "); Serial.println(httpResponseCode);
-            }
-            http.end();
-            return responsePayload;    
-        };
+  private:
+    String DATABASE_URL = "https://enitiotreasurehunt.link/";
+    String DATABASE_DEBUG_URL = "http://enitiotreasurehunt.link/";
+    bool DEBUG = true;  // HTTPS not supported yet
+    String GET_Request(String route) {
+      HTTPClient http;
+      String url;
+      http.setTimeout(HTTP_TIMEOUT);
+      if (!DEBUG) {
+        url = DATABASE_URL + route;
+      } else {
+        url = DATABASE_DEBUG_URL + route;
+        Serial.print("DEBUG GET "); Serial.println(url);
+      }
+      http.begin(url.c_str());
 
-        GAME_CONSTANTS retrieveGameConstantsFromJSONArray(String json_array) {
-            JSONVar json_obj = JSON.parse(json_array);
-            GAME_CONSTANTS game_const;
-            if (JSON.typeof(json_obj) == "undefined") {
-                Serial.println("Parsing input failed!");
-                return game_const;
-            }
-            game_const.REVIVAL_STATION_INITIAL_HP = JSON.stringify(json_obj["REVIVAL_STATION_INITIAL_HP"]).toInt();
-            game_const.REVIVAL_STATION_ACTION_RECV_WAIT = JSON.stringify(json_obj["REVIVAL_STATION_ACTION_RECV_WAIT"]).toInt();
-            game_const.REVIVAL_STATION_RECOVER_DURATION = JSON.stringify(json_obj["REVIVAL_STATION_RECOVER_DURATION"]).toInt();
-            game_const.REVIVAL_STATION_MULTIPLIER = JSON.stringify(json_obj["REVIVAL_MULTIPLIER"]).toInt();
-            game_const.HTTP_TIMEOUT = JSON.stringify(json_obj["HTTP_TIMEOUT"]).toInt();
-            return game_const;
-        };
-    
-    public:
-        bool connectToWiFi() {
-            // returns True if connected, False if timeout
-            // WiFi.begin(HOME_WIFI_SSID, HOME_WIFI_PASSWORD);
-            WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_ANONYMOUS_IDENTITY, EAP_IDENTITY, EAP_PASSWORD);
-            Serial.print("Connecting to Wi-Fi");
-            int counter = 0;
-            while (WiFi.status() != WL_CONNECTED) {
-                Serial.print(".");
-                delay(500);
-                counter++;
-                if (counter >= 60) {
-                    // timeout
-                    return false;
-                }
-            };
-            Serial.print("Connected with IP: "); Serial.println(WiFi.localIP());
-            return true;
-        };
+      int httpResponseCode = http.GET();
+      String payload = "{}";
 
-        int hasGameStarted() {
-            String url = "game_status";
-            String jsonArray = GET_Request(url);
-            return retrieveParameterFromJSONArray("has_game_started", jsonArray).toInt();
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: "); Serial.println(httpResponseCode);
+        payload = http.getString();
+      } else {
+        Serial.print("Error code: "); Serial.println(httpResponseCode);
+      }
+      http.end();
+      return payload;
+    };
+
+    String retrieveParameterFromJSONArray(String parameter, String json_array) {
+      JSONVar json_obj = JSON.parse(json_array);
+      if (JSON.typeof(json_obj) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return "";
+      }
+      return JSON.stringify(json_obj[parameter]);
+    };
+
+    String POST_Request(String route, const char* payload) {
+      HTTPClient http;
+      http.setTimeout(HTTP_TIMEOUT);
+      String url;
+      if (!DEBUG) {
+        url = DATABASE_URL + route;
+      } else {
+        url = DATABASE_DEBUG_URL + route;
+        Serial.print("DEBUG POST "); Serial.println(url);
+      }
+      
+      http.begin(url.c_str());
+      Serial.println(url);
+      
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST(payload);
+      String responsePayload = "{}";
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: "); Serial.println(httpResponseCode);
+        responsePayload = http.getString();
+      }
+      else {
+        Serial.print("Error code: "); Serial.println(httpResponseCode);
+      }
+      http.end();
+      return responsePayload;
+    };
+
+    GAME_CONSTANTS retrieveGameConstantsFromJSONArray(String json_array) {
+      JSONVar json_obj = JSON.parse(json_array);
+      GAME_CONSTANTS game_const;
+      if (JSON.typeof(json_obj) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return game_const;
+      }
+      game_const.REVIVAL_STATION_INITIAL_HP = JSON.stringify(json_obj["REVIVAL_STATION_INITIAL_HP"]).toInt();
+      game_const.REVIVAL_STATION_ACTION_RECV_WAIT = JSON.stringify(json_obj["REVIVAL_STATION_ACTION_RECV_WAIT"]).toInt();
+      game_const.REVIVAL_STATION_RECOVER_DURATION = JSON.stringify(json_obj["REVIVAL_STATION_RECOVER_DURATION"]).toInt();
+      game_const.REVIVAL_STATION_MULTIPLIER = JSON.stringify(json_obj["REVIVAL_MULTIPLIER"]).toInt();
+      game_const.HTTP_TIMEOUT = JSON.stringify(json_obj["HTTP_TIMEOUT"]).toInt();
+      return game_const;
+    };
+
+  public:
+    bool connectToWiFi() {
+      // returns True if connected, False if timeout
+      // WiFi.begin(HOME_WIFI_SSID, HOME_WIFI_PASSWORD);
+      WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_ANONYMOUS_IDENTITY, EAP_IDENTITY, EAP_PASSWORD);
+      Serial.print("Connecting to Wi-Fi");
+      int counter = 0;
+      while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+        counter++;
+        if (counter >= 60) {
+          // timeout
+          return false;
         }
+      };
+      Serial.print("Connected with IP: "); Serial.println(WiFi.localIP());
+      return true;
+    };
 
-        GAME_CONSTANTS getGameConstants() {
-            String url = "get_game_constants";
-            String jsonArray = GET_Request(url.c_str());
-            return retrieveGameConstantsFromJSONArray(jsonArray);
-        };
+    int hasGameStarted() {
+      String url = "game_status";
+      String jsonArray = GET_Request(url);
+      return retrieveParameterFromJSONArray("has_game_started", jsonArray).toInt();
+    }
+
+    GAME_CONSTANTS getGameConstants() {
+      String url = "get_game_constants";
+      String jsonArray = GET_Request(url.c_str());
+      return retrieveGameConstantsFromJSONArray(jsonArray);
+    };
 };
 
 DBConnection dbc;
--- RevivalStation/RevivalStation.ino
+++ RevivalStation/RevivalStation.ino
@@ -1,12 +1,9 @@
-// upload this code whenever you want to start testing
-// comment out the clearEEPROM() function when used on real game
-
 #include <EEPROM.h>
 #include "ENITIO_ir.h"
 #include "ENITIO_enums.h"
-#include "ENITIO_ESPNOW.h"
 #include "ENITIO_NeoPixel.h"
 #include "ENITIO_joystick.h"
+#include "ENITIO_wifi.h"
 
 #include <SPI.h>
 #include <Wire.h>
@@ -17,7 +14,7 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
+// The pins for I2C are defined by the Wire-library.
 // On an arduino UNO:       A4(SDA), A5(SCL)
 // On an arduino MEGA 2560: 20(SDA), 21(SCL)
 // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
@@ -29,7 +26,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define EEPROM_SIZE 10
 
-#define ENABLE_add 0  // 0 means Treasure has not been initialized, 1 means already initialized
+#define ENABLE_add 0  // 1 means revival station is running, 2 means disabled (No HP, recovering)
 
 #define R_ON 255
 #define G_ON 0
@@ -41,6 +38,33 @@ int REVIVAL_STATION_ACTION_RECV_WAIT;
 int REVIVAL_STATION_RECOVER_DURATION;
 int REVIVAL_MULTIPLIER;
 
+TaskHandle_t backgroundTask;
+bool parameters_updated = false;
+
+void backgroundTaskCode(void *pvParameters) {
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  for (;;) {
+    if (!parameters_updated) {
+      Serial.println("[BACKGROUND] Retrieving Latest Game Constants from Server...");
+      bool isWiFiConnected = dbc.connectToWiFi();
+      if (isWiFiConnected) {
+        GAME_CONSTANTS game_consts = dbc.getGameConstants();
+        HTTP_TIMEOUT = game_consts.HTTP_TIMEOUT;
+        REVIVAL_STATION_INITIAL_HP = game_consts.REVIVAL_STATION_INITIAL_HP;
+        REVIVAL_STATION_ACTION_RECV_WAIT = game_consts.REVIVAL_STATION_ACTION_RECV_WAIT;
+        REVIVAL_STATION_RECOVER_DURATION = game_consts.REVIVAL_STATION_RECOVER_DURATION;
+        REVIVAL_MULTIPLIER = game_consts.REVIVAL_STATION_MULTIPLIER;
+        parameters_updated = true;
+        Serial.println("[BACKGROUND] Game Constants Updated Successfully");
+      } else {
+        Serial.println("[BACKGROUND] Failed to connect to WiFi, retrying..");
+      }
+    } else {
+      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+    }
+  }
+};
 
 class RevivalStation
 {
@@ -48,24 +72,26 @@ class RevivalStation
     int HP;
     unsigned long last_revival_request = 0;
     unsigned long timeleftToRecover = 0;
-    
-  public:
-    
-    int CLAN_, ID_, En_, action_; 
 
-    void init_treasure(){
+  public:
+
+    int CLAN_, ID_, En_, action_;
+
+    void init_treasure() {
       EEPROM.write(ENABLE_add, 1);
       EEPROM.commit();
       HP = REVIVAL_STATION_INITIAL_HP;
     };
 
-    void RevivalLoop(){
+    void RevivalLoop() {
       int currStatus = EEPROM.read(ENABLE_add);
       unsigned long currTime = millis();
-      if (currStatus == 1){
+      if (currStatus == 1) {
         handleJoystick();
+        display_in_game();
       }
-      else if (currStatus == 2){
+      else {
+        // currStatus == 2
         timeleftToRecover = max(int(REVIVAL_STATION_RECOVER_DURATION - (currTime - last_revival_request)), 0);
         display_no_hp(timeleftToRecover);
         if (timeleftToRecover == 0) {
@@ -79,43 +105,42 @@ class RevivalStation
       }
     }
 
-    void handleJoystick(){
+    void handleJoystick() {
       joystick_pos joystick_pos = RevivalStation_joystick.read_Joystick();
       if (RevivalStation_joystick.get_state() == 0) {
-        switch (joystick_pos){
-            case button:
-              revive_player();
-              RevivalStation_joystick.set_state();
-              HP--;
-              Serial.print(HP); Serial.println(" Before Station Closes");
-              if (HP == 0) {
-                  Serial.println("Closing Revival Station");
-                  RevivalStation_NeoPixel.off_FRONT();
-                  RevivalStation_NeoPixel.off_TOP();
-                  EEPROM.write(ENABLE_add, 2);
-                  EEPROM.commit();
-                  last_revival_request = millis();
-              }
-              break;
+        switch (joystick_pos) {
+          case button:
+            revive_player();
+            RevivalStation_joystick.set_state();
+            HP--;
+            Serial.print(HP); Serial.println(" Before Station Closes");
+            if (HP == 0) {
+              Serial.println("Closing Revival Station");
+              RevivalStation_NeoPixel.off_FRONT();
+              RevivalStation_NeoPixel.off_TOP();
+              EEPROM.write(ENABLE_add, 2);
+              EEPROM.commit();
+              last_revival_request = millis();
+            }
+            break;
 
-            case idle:
-              break;
+          case idle:
+            break;
 
-            default:
-              RevivalStation_joystick.set_state();
-              break;
-          }
+          default:
+            RevivalStation_joystick.set_state();
+            break;
+        }
       }
     }
 
-    void revive_player(){
+    void revive_player() {
       uint16_hex_digits address_digits, command_digits;
 
       address_digits.digit0 = 0;
       address_digits.digit2 = 0;
 
       command_digits.digit0 = revive;
-
       command_digits.digit1 = REVIVAL_MULTIPLIER;
 
       ir_signal send_signal;
@@ -124,91 +149,66 @@ class RevivalStation
       send_signal.command = command_digits;
 
       RevivalStation_IR.send(send_signal, 1);
-    } ;
+    };
 
-    void display_not_playing_yet(){
-      display.clearDisplay();
+    void display_in_game() {
+      display.clearDisplay(); \
       display.setTextSize(1); // Draw SIZE
       display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
       display.setCursor(0, 0);
-      display.println(F("   Revival Station  ")); 
-          
-      display.setCursor(0, 16);
+      display.println(F("   Revival Station  "));
+
+      display.setCursor(0, 20);
+      display.setTextSize(1);      // Normal 1:1 pixel scale
       display.setTextColor(SSD1306_WHITE); // Draw white text
-      display.println("      Hold on!!     ");
-      display.setCursor(0, 30);
-      display.println("  The game has not  ");
-      display.println("    started yet.    ");
+      display.println("  Put your WANDERer  ");
+      display.setCursor(0, 32);
+      display.println(" towards the Station ");
+      display.setCursor(0, 44);
+      display.println("      and PRESS!     ");
+
+      if (!parameters_updated) {
+        display.setCursor(0, 56);
+        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+        display.println("    OFFLINE MODE    ");
+      }
+      
       display.display();
     };
 
-    void display_in_game(){
-      int currStatus = EEPROM.read(ENABLE_add);
-      if(currStatus == 1) {
-        display.clearDisplay();\
-        display.setTextSize(1); // Draw SIZE
-        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-        display.setCursor(0, 0);
-        display.println(F("   Revival Station  ")); 
-
-        display.setCursor(0, 20);
-        display.setTextSize(1);      // Normal 1:1 pixel scale
-        display.setTextColor(SSD1306_WHITE); // Draw white text
-        display.println("  Put your WANDERer  ");
-        display.setCursor(0, 32);
-        display.println(" towards the Station ");
-        display.setCursor(0, 44);
-        display.println("      and PRESS!     ");
-      
-        display.display();
-      }
-    };
-
-    void display_no_hp(int this_timeleftToRecover){
+    void display_no_hp(int this_timeleftToRecover) {
       display.clearDisplay();
       display.setTextSize(1); // Draw SIZE
       display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
       display.setCursor(0, 0);
-      display.println(F("   Revival Station  ")); 
-          
+      display.println(F("   Revival Station  "));
+
       display.setCursor(0, 24);
-      display.setTextColor(SSD1306_WHITE); // Draw white text              |  
+      display.setTextColor(SSD1306_WHITE); // Draw white text              |
       display.println("     Out of HP      ");
 
       display.setCursor(0, 40);
       display.print("Come back in: ");
-      display.println(int(this_timeleftToRecover/1000.0)+1);
+      display.println(int(this_timeleftToRecover / 1000.0) + 1);
+
+      if (!parameters_updated) {
+        display.setCursor(0, 56);
+        display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+        display.println("    OFFLINE MODE    ");
+      }
+      
       display.display();
     };
 
 };
 
 RevivalStation revival_station;
-bool gameStarted = 0;
 
-void clearEEPROM(){
+void clearEEPROM() {
   int i;
-  for (i=0; i<EEPROM_SIZE; i++){
-    EEPROM.write(i,0);
+  for (i = 0; i < EEPROM_SIZE; i++) {
+    EEPROM.write(i, 0);
   }
-}
-
-int get_game_state(){
-      // retrieve game state from server
-      // 0 mean game did not start
-      // 1 mean in game
-      // once the game has started then we dunnid to check anymore
-   
-   if (!gameStarted) {
-     gameStarted = dbc.hasGameStarted();
-     Serial.print("Game Status: "); Serial.println(gameStarted);
-     if(gameStarted) {
-       RevivalStation_NeoPixel.displayRGB_FRONT(R_ON, B_ON, G_ON);
-       RevivalStation_NeoPixel.displayRGB_TOP(R_ON, B_ON, G_ON);
-       revival_station.init_treasure();
-     }
-     return gameStarted;
-   } else return 1;
 }
 
 void setup() {
@@ -220,44 +220,34 @@ void setup() {
   EEPROM.begin(EEPROM_SIZE);
   clearEEPROM();
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-        Serial.println(F("SSD1306 allocation failed"));
-        for(;;); // Don't proceed, loop forever
-      }
-
-  RevivalStation_EspNOW.enable();
-  
-  bool isWiFiConnected = dbc.connectToWiFi();
-  while (!isWiFiConnected) {
-    Serial.println("Reconnecting..");
-    isWiFiConnected = dbc.connectToWiFi();
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
   }
 
-  GAME_CONSTANTS game_consts = dbc.getGameConstants();
-  HTTP_TIMEOUT = game_consts.HTTP_TIMEOUT;
-  REVIVAL_STATION_INITIAL_HP = game_consts.REVIVAL_STATION_INITIAL_HP;
-  REVIVAL_STATION_ACTION_RECV_WAIT = game_consts.REVIVAL_STATION_ACTION_RECV_WAIT;
-  REVIVAL_STATION_RECOVER_DURATION = game_consts.REVIVAL_STATION_RECOVER_DURATION;
-  REVIVAL_MULTIPLIER = game_consts.REVIVAL_STATION_MULTIPLIER;
+  // Hardcoded constants, in case there is no WiFi to update
+  HTTP_TIMEOUT = 30000;
+  REVIVAL_STATION_INITIAL_HP = 30;
+  REVIVAL_STATION_ACTION_RECV_WAIT = 3000;
+  REVIVAL_STATION_RECOVER_DURATION = 20000;
+  REVIVAL_MULTIPLIER = 5;
+
+  // initialise treasure - so that it can be used even in offline mode
+  RevivalStation_NeoPixel.displayRGB_FRONT(R_ON, B_ON, G_ON);
+  RevivalStation_NeoPixel.displayRGB_TOP(R_ON, B_ON, G_ON);
+  revival_station.init_treasure();
+
+  // Check for game constant updates on the 2nd core
+  xTaskCreatePinnedToCore(
+    backgroundTaskCode,   // Task function.
+    "backgroundTask",     // name of task.
+    10000,                // Stack size of task
+    NULL,                 // parameter of the task
+    2,                    // priority of the task
+    &backgroundTask,      // Task handle to keep track of created task
+    0);
 }
 
 void loop() {
-  // First check if ESP is connected to WiFi
-  if (WiFi.status() != WL_CONNECTED) {
-    bool isWiFiConnected = dbc.connectToWiFi();
-    if (!isWiFiConnected) {
-        // timeout
-        ESP.restart();
-    }
-  }
-  switch (get_game_state()){
-    case 0:
-      revival_station.display_not_playing_yet();
-      delay(10000);
-      break;
-    case 1:
-      revival_station.RevivalLoop();
-      revival_station.display_in_game();
-      break;
-  }
+  revival_station.RevivalLoop();
 }
