79872c78f443deec3092f2d6543ccf51ca898f0c
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2024-07-31 12:29:05 +0800

    Reformat code for consistency

--- WANDERER_main/WANDERER_main.ino
+++ WANDERER_main/WANDERER_main.ino
@@ -15,48 +15,58 @@
 #include "MACAddress.h"
 #define LED_BUILTIN 2
 
-
-
 TaskHandle_t backgroundTask;
 
+void backgroundTaskCode(void *pvParameters)
+{
+  TickType_t xLastWakeTime = xTaskGetTickCount();
 
-void backgroundTaskCode(void *pvParameters) {
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-
-    for (;;) {
-        if (currentProcess == TreasureHuntProcess) {
-            if (!PLAYER.gameStarted) {
-                game_started_buffer = dbc.hasGameStarted();
-            } else if (WIFI_ON) {
-                PLAYER.gameBackgroundProcess();
-            } else {
-              vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
-            }
-        } else {
-            vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
-        }
+  for (;;)
+  {
+    if (currentProcess == TreasureHuntProcess)
+    {
+      if (!PLAYER.gameStarted)
+      {
+        game_started_buffer = dbc.hasGameStarted();
+      }
+      else if (WIFI_ON)
+      {
+        PLAYER.gameBackgroundProcess();
+      }
+      else
+      {
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+      }
     }
+    else
+    {
+      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+    }
+  }
 };
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   EEPROM.begin(EEPROM_SIZE);
   Player_NeoPixel.initialize();
 
   // check OLED display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
 
   StartUpDisplay();
-      
-  // initialize Joystick 
+
+  // initialize Joystick
   Player_joystick.initialize();
 
   // initialize IR
   Player_IR.enable();
-      
+
   Player_EspNOW.enable();
 
   my_MAC_address = WiFi.macAddress();
@@ -76,45 +86,51 @@ void setup() {
   MAX_ATTACK_MULTIPLIER = 3;
   MAX_COLLECT_MULTIPLIER = 10;
   BOMB_HP_DEDUCTION = 6;
-  KILL_UPDATE_SERVER_INTERVAL = 10 * 60 * 1000;  // 10 mins
+  KILL_UPDATE_SERVER_INTERVAL = 10 * 60 * 1000; // 10 mins
   WIFI_ON = EEPROM.read(ONLINE_mode_add);
-    
-  Serial.print("Actual MAC Address: "); Serial.println(my_MAC_address);
+
+  Serial.print("Actual MAC Address: ");
+  Serial.println(my_MAC_address);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
+  if (EEPROM.read(PLAYER_enable_add) == 0) // will only run after fac. reset or once on the very FIRST flash 
+    EEPROM.write(CLAN_add, 255); // 255 to set clan to unknown (default 0 is invicta)
+
   xTaskCreatePinnedToCore(
-                      backgroundTaskCode,   // Task function. 
-                      "backgroundTask",     // name of task. 
-                      10000,       // Stack size of task
-                      NULL,        // parameter of the task
-                      2,           // priority of the task
-                      &backgroundTask,      // Task handle to keep track of created task
-                      0);
+      backgroundTaskCode, // Task function.
+      "backgroundTask",   // name of task.
+      10000,              // Stack size of task
+      NULL,               // parameter of the task
+      2,                  // priority of the task
+      &backgroundTask,    // Task handle to keep track of created task
+      0);
 }
 
-void loop() {
-  switch (currentProcess) {
-    case MainMenuProcess:
-      My_MainMenu.MainMenuLoop();
-      break;
-    case TreasureHuntProcess:
-      PLAYER.gameMainLoop();
-      break;
-    case ProfileProcess:
-      My_Profile.ProfileLoop();
-      break;
-    case AdminProcess:
-      My_Admin.AdminLoop();
-      break;
-    case CreditProcess:
-      My_Credits.CreditsLoop();
-      break;
-    case MACAddressProcess:
-      My_MACAddress.MACAddressLoop();
-      break;
-    default:
-      currentProcess = MainMenuProcess;
-      break;
+void loop()
+{
+  switch (currentProcess)
+  {
+  case MainMenuProcess:
+    My_MainMenu.MainMenuLoop();
+    break;
+  case TreasureHuntProcess:
+    PLAYER.gameMainLoop();
+    break;
+  case ProfileProcess:
+    My_Profile.ProfileLoop();
+    break;
+  case AdminProcess:
+    My_Admin.AdminLoop();
+    break;
+  case CreditProcess:
+    My_Credits.CreditsLoop();
+    break;
+  case MACAddressProcess:
+    My_MACAddress.MACAddressLoop();
+    break;
+  default:
+    currentProcess = MainMenuProcess;
+    break;
   }
 }
